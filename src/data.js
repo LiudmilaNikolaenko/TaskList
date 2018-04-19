4d144d2f3941cadcfc266a818df470f478e6e04e
Liudmila Nikolaenko <lldubinina@mail.ru>
2018-04-19 13:27:00 +0300

    new files

--- src/data.js
+++ src/data.js
@@ -1,30 +1,20 @@
 export const tasklist = ["56c782f18990ecf954f6e027", "56c782f17b4e0ba78c7ad717", 
-"56c782f1978fdf9a0100df52", "56c782f1e17f4f9311dfaa2c", "56c782f197fe2bad471b3740"];
+"56c782f1978fdf9a0100df52"];
 
 export const tasks = [
   {
     "id": "56c782f18990ecf954f6e027",
-    "text": "1. addTask.",
+    "text": "First task.",
     "completed": false,
   },
   {
     "id": "56c782f17b4e0ba78c7ad717",
-    "text": "2. deleteTask.",
+    "text": "Second task.",
     "completed": false,
   },
   {
     "id": "56c782f1978fdf9a0100df52",
-    "text": "3. markTask.",
-    "completed": false,
-  },
-  {
-    "id": "56c782f1e17f4f9311dfaa2c",
-    "text": "4. setTimer.",
-    "completed": false,
-  },
-  {
-    "id": "56c782f197fe2bad471b3740",
-    "text": "5. sortTaskList.",
+    "text": "Third task.",
     "completed": false,
   }
 ];
